d46bd9e7f6d37ea7610b09c93a4b42c7a40e5d28
goliverosdiazbot <goliverosdiaz@gmail.com>
2025-03-20 17:15:12 -0300

    App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardMovistar from './components/DashboardMovistar';
+
+function App() {
+  return (
+    <div className="App">
+      <DashboardMovistar />
+    </div>
+  );
+}
+
+export default App;
